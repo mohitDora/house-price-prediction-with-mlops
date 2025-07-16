82e907d4d37c40fee2591c31c71f331111865695
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-07-16 15:44:03 +0530

    logger and exception done

---
+++ demo.py
@@ -0,0 +1,16 @@
+# from src.logger import logger
+
+# logger.debug("This is a debug message.")
+# logger.info("This is an info message.")
+# logger.warning("This is a warning message.")
+# logger.error("This is an error message.")
+# logger.critical("This is a critical message.")
+
+# from src.exception import MyException
+# import sys
+
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logger.info(e)
+#     raise MyException(e, sys) from e
